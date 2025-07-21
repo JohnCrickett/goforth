29ddcb357ed98deed537dfee9985db8d8a06e4d8
John Crickett <3667898+JohnCrickett@users.noreply.github.com>
2025-07-21 22:45:20 +0100

    Refactor and improve tests, add stack ops

--- interpreter/interpreter_test.go
+++ interpreter/interpreter_test.go
@@ -4,11 +4,12 @@ import (
 	"testing"
 )
 
-func TestNumbers(t *testing.T) {
+func TestInterpret(t *testing.T) {
 	tests := map[string]struct {
 		input    string
 		expected []int
 	}{
+		// Numbers
 		"1": {
 			input:    "1\n",
 			expected: []int{1},
@@ -17,34 +18,7 @@ func TestNumbers(t *testing.T) {
 			input:    "1 2 3",
 			expected: []int{1, 2, 3},
 		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			interpreter := NewInterpreter(test.input)
-
-			for {
-				w, err := interpreter.Word()
-				if err != nil {
-					break
-				}
-				interpreter.Interpret(w)
-			}
-
-			for i := range len(test.expected) {
-				if interpreter.stack.items[i] != test.expected[i] {
-					t.Errorf("expected %v, got %v", test.expected, interpreter.stack)
-				}
-			}
-		})
-	}
-}
-
-func TestMathOperations(t *testing.T) {
-	tests := map[string]struct {
-		input    string
-		expected []int
-	}{
+		// Mathematical ops
 		"add": {
 			input:    "1 2 +",
 			expected: []int{3},
@@ -64,6 +38,31 @@ func TestMathOperations(t *testing.T) {
 		"subtract": {
 			input:    "2 1 -",
 			expected: []int{1},
+		},
+		"mod": {
+			input:    "3 2 mod",
+			expected: []int{1},
+		},
+		// Stack ops
+		"swap": {
+			input:    "3 2 swap",
+			expected: []int{2, 3},
+		},
+		"dup": {
+			input:    "3 2 2 dup",
+			expected: []int{3, 2, 2},
+		},
+		"over": {
+			input:    "3 2 over",
+			expected: []int{3, 2, 3},
+		},
+		"rot": {
+			input:    "3 2 1 rot",
+			expected: []int{2, 1, 3},
+		},
+		"drop": {
+			input:    "3 2 1 0 drop",
+			expected: []int{3, 2, 1},
 		},
 	}
 
